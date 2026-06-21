8bfd97bbe3aef39513b47b7068d4659ee4d9fc3d
Axel-A-77 <rai.rsm.17@gmail.com>
2026-06-21 09:34:02 -0500

    Connect corrected hospital and kiosk scene

--- src/scenes/allV2.tsx
+++ src/scenes/allV2.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {SCENES as BASE_SCENES, SceneProps} from './all';
 import {S04CandidateScene} from './S04CandidateScene';
 import {AlcaldeSceneV2} from './AlcaldeSceneV2';
+import {S26WorkSceneV2} from './S26WorkSceneV2';
 import {S31HonestySceneV2} from './S31HonestySceneV2';
 
 export const SCENES: Record<string, React.FC<SceneProps>> = {
   ...BASE_SCENES,
   '04_politico_flaco_palabras': S04CandidateScene,
   alcalde: AlcaldeSceneV2,
+  '26_kiosko_hospital': S26WorkSceneV2,
   '31_diogenes_lampara': S31HonestySceneV2,
 };
